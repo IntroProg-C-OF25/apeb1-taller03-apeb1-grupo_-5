966b4470388fc8dee970636bfc9b4e578fe400d3
Julian-2606 <julianfjs1982@gmail.com>
2024-10-24 12:30:38 -0500

    Resolucion del ejercicio 2

--- ProyectoTaller3/src/Ejercicio10.java
+++ ProyectoTaller3/src/Ejercicio10.java
@@ -8,9 +8,5 @@
  * @author julia
  */
 public class Ejercicio10 {
-    public static void main(String[] args) {
-        double respuesta;
-        respuesta = ((1+2+4+3)/5) + Math.sqrt(81);
-        System.out.println("respuesta = " + respuesta);
-    }
+
 }
--- ProyectoTaller3/src/Ejercicio2.java
+++ ProyectoTaller3/src/Ejercicio2.java
@@ -8,5 +8,9 @@
  * @author julia
  */
 public class Ejercicio2 {
-    
+    public static void main(String[] args) {
+        double respuesta;
+        respuesta = Math.pow(10,2) + 10;
+        System.out.println("respuesta = " + respuesta);
+    }
 }
